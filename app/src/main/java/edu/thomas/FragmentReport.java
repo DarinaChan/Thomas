ed93eb2a4d84e4719300cfe959eee4ceb519232f
Baptiste Royer <baptiste.royer@etu.unice.fr>
2024-05-14 16:12:36 +0200

    Ajout de la vue pour les incidents

--- app/src/main/java/edu/thomas/FragmentReport.java
+++ app/src/main/java/edu/thomas/FragmentReport.java
@@ -4,16 +4,36 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FragmentReport extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        return inflater.inflate(R.layout.fragment_report, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_report2, container, false);
+
+        Spinner spinner = rootView.findViewById(R.id.incident_spinner);
+
+        List<String> incidents = new ArrayList<>();
+        incidents.add("Pick pocket");
+        incidents.add("Passager dangereux");
+        incidents.add("Train en retard");
+        incidents.add("Train à l heure");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_item, incidents);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+
+        return rootView;
     }
 }
