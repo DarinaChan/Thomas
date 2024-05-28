69c86f9df5c612fdf93e96856781a94e169039a7
QuentinELLEON <quentin.elleon@etu.unice.fr>
2024-05-28 15:43:44 +0200

    Incidents db fix

--- app/src/main/java/edu/thomas/model/incident/Incident.java
+++ app/src/main/java/edu/thomas/model/incident/Incident.java
@@ -1,12 +1,19 @@
 package edu.thomas.model.incident;
 
+import java.util.Optional;
+
+import edu.thomas.Database;
+
 public abstract class Incident {
     private TypeOfIncident type; // No SetType because it's only set at the initialization
     private String description;
+    private String id;
+    Database db = new Database();
 
     public Incident(TypeOfIncident type,String description){
         this.type = type;
         setDescription(description);
+        this.id = db.getIdForIncident();
     }
     public void setDescription(String description) {
         this.description = description;
@@ -20,6 +27,9 @@ public abstract class Incident {
     }
     public void notifyUsers(){
         // Notify everyone
+    }
+    public int getTypeId(){
+        return this.type.ordinal();
     }
 }
 
